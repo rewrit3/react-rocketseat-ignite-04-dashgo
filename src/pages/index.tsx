9f59d1efa910101a84b9dd3b6e830f461b733e61
Adolfo Cruz <adolfo@rewrite.com.br>
2022-04-27 17:53:35 -0400

    feat: index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,74 @@
+import {
+  Flex,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  Button }
+from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      justify="center"
+      align="center"
+    >
+      <Flex
+        as="form"
+        flexDir="column"
+        w="100%"
+        maxW={360}
+        p="8"
+        borderRadius={8}
+        bgColor="gray.800"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              size="lg"
+              variant="filled"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              size="lg"
+              variant="filled"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          size="lg"
+          colorScheme="pink"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
